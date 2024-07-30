b127424bbd5562084e7e1f77e6cdd09b3e4a8318
Faizan Asad <faizan.asad@merisehat.app>
2024-07-30 10:48:08 +0500

    furniture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // create all pages same design becuase i dont have design for each page
+  
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'shop', component: ShopPageComponent },
+  { path: 'contact', component: ContactPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { MainComponent } from './main/main.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { BannerComponent } from './banner/banner.component';
+import { BrowseRangeComponent } from './browse-range/browse-range.component';
+import { OurProductComponent } from './our-product/our-product.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentAreaComponent,
+    MainComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    BannerComponent,
+    BrowseRangeComponent,
+    OurProductComponent,
+    ShopPageComponent,
+    ContactPageComponent,
+    ShopPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
